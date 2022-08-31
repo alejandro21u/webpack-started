6de5fa914f1b710bb9fba6cc0b5a562f31587d9b
Alejandro~ <Alejandro.urtubia30@gmail.com>
2022-08-31 09:07:55 -0400

    proyecto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//para importar cosas desde otro repositorio se ocupa el import y entre los corchetes se llama a la funcion que quieras ocupar,el from busca la ubicacion de donde esta el archivo
+import {saludar} from './js/funciones.js'
+import './styles.css';
+
+
+const nombre = 'Pedro'
+saludar(nombre);
